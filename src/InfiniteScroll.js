02aa7680eec596464b673b3003f3b772f58fa46c
Patrick Brown <patrick.barry.brown@gmail.com>
2021-12-10 09:58:24 -0700

    debug

--- src/InfiniteScroll.js
+++ src/InfiniteScroll.js
@@ -210,7 +210,7 @@ export default class InfiniteScroll extends Component {
     } else {
       offset = el.scrollHeight - parentNode.scrollTop - parentNode.clientHeight;
     }
-
+    console.log(offest)
     // Here we make sure the element is visible as well as checking the offset
     if (
       offset < Number(this.props.threshold) &&
